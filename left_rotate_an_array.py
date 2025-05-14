f3614e8c39b220addd9f6598aa301774e73e5b8a
Parthiv-06 <puthalapattuparthiv@gmail.com>
2025-05-14 20:58:41 +0530

    updated left rotate an array

--- left_rotate_an_array.py
+++ left_rotate_an_array.py
@@ -1,10 +1,10 @@
 def left_rotate_an_array(arr,k):
     n=len(arr)
     temp=[0]*n
-    for i in range(k,n):
-        temp[i-k]=arr[i]
+    for i in range(n-k):
+        temp[i]=arr[i+k]
     for j in range(k):
-        temp[k+j+1]=arr[j]
+        temp[(n-k)+j]=arr[j]
     return temp
 arr=[1,2,3,4,5,6,7]
-print(left_rotate_an_array(arr,5))
+print(left_rotate_an_array(arr,2))
